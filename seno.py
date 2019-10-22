24d1468740b37c2f41da6f5cd37080b67ff5b1f1
Default user <efuentesrico@gmail.com>
2019-10-22 18:27:41 +0000

    seno.py

---
+++ seno.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+t = np.linspace(0,2*np.pi,2000)
+
+y = np.sin(t)
+
+plt.figure()
+plt.plot(t,y)
+plt.xlabel("t")
+plt.ylabel("sin(t)")
+plt.savefig("seno")
